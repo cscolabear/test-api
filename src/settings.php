fb0079c0fdaa55ded5c19e3294ae00034e382842
Cola <cscolabear@gmail.com>
2017-06-04 00:03:53 +0800

    add db config and connect

--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,16 @@ return [
             'path' => __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        'db' => [
+            'driver'    => 'mysql',
+            'host'      => getenv('DB_HOST'),
+            'database'  => getenv('DB_DATABASE'),
+            'username'  => getenv('DB_USERNAME'),
+            'password'  => getenv('DB_PASSWORD'),
+            'charset'   => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix'    => '',
+        ]
     ],
 ];
