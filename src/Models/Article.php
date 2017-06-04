34fb5350d0a9346c8837aad4e912bf822273a307
Cola <cscolabear@gmail.com>
2017-06-04 12:06:36 +0800

    add field to Article model

--- src/Models/Article.php
+++ src/Models/Article.php
@@ -6,4 +6,5 @@ use \Illuminate\Database\Eloquent\Model;
 
 class Article extends Model {
     protected $table = 'articles';
+    protected $fillable = ['title', 'description', 'image', 'content'];
 }
